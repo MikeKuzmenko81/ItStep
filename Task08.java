e659eb0cfba6668f3fa7362f3e21f92d5d2c18cc
Mike_Kuzzz <mike.kmv@gmail.com>
2019-11-11 00:34:23 +0300

    Task08

--- Task08.java
+++ Task08.java
@@ -1,7 +1,14 @@
 package ru.mike;
 
+import java.util.Scanner;
+
 public class Task08 {
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        double spedOfSound = 343.1; //скорость звука в м/с при 20 град по Целс
+        System.out.println("Сколько времени в секундах прошло между вспышкой молнии и раскатом грома :");
+        final double time = scan.nextDouble(); //время в секундах, между вспышкой и раскатом грома
+        double s = spedOfSound * time;
+        System.out.println("Расстоянии от молнии в метрах : " + s);
     }
 }
