fb76020e3082d9c05546fdb4ef04ea8f6ef71d5f
Mike_Kuzzz <mike.kmv@gmail.com>
2019-11-11 00:54:05 +0300

    Task12

---
+++ Task12.java
@@ -0,0 +1,15 @@
+package ru.mike;
+
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Напишите сколько человек в перых классах :");
+        int n = scan.nextInt();
+        int pirogkov = 1;
+        int stakanMoloka = 0;
+        int melkiyUchenik = 10;
+
+    }
+}
