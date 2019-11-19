a0cec810827cd0333ba8c020fab15c58244688a0
Mike_Kuzzz <mike.kmv@gmail.com>
2019-11-19 15:25:41 +0300

    Chek NUMBER add

--- praktik/lab06/Task01.java
+++ praktik/lab06/Task01.java
@@ -9,32 +9,47 @@ public class Task01 {
         String str = scan.nextLine();
         //проверить что в веденной строке 6 символов
         //каждый введеный символ это число
+        char leftBord = '0';
+        char rightBord = '9';
+        String checkStr = "";
         //if(str.matches("/d{6}/ui")){
+
         int sumLeft = 0;
         int sumRight = 0;
         String strLeft = "";
         String strRight = "";
         if(str.length() == 6){
-            //начинаем считать
             for (int i = 0; i < str.length(); i++){
-               int num = Integer.parseInt(str.substring(i, i+1));
-               if(i < 3){
-                   sumLeft += num;
-                   strLeft += num + " + ";
-               }else {
-                   sumRight += num;
-                   strRight += num + " + ";
-               }
+                if(str.charAt(i) >= leftBord && str.charAt(i) <= rightBord){
+                    checkStr += str.charAt(i);
+                }
             }
-            if(sumLeft == sumRight){
-                System.out.println(strLeft + " == " + strRight);
-                System.out.printf("Число %s счастливое", str);
+            if(str.equals(checkStr)) {
+                //начинаем считать
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) >= leftBord && str.charAt(i) <= rightBord) {
+                    }
+                    int num = Integer.parseInt(str.substring(i, i + 1));
+                    if (i < 3) {
+                        sumLeft += num;
+                        strLeft += num + " + ";
+                    } else {
+                        sumRight += num;
+                        strRight += num + " + ";
+                    }
+                }
+                if (sumLeft == sumRight) {
+                    System.out.println(strLeft + " == " + strRight);
+                    System.out.printf("Число %s счастливое", str);
+                } else {
+                    System.out.println(strLeft + " != " + strRight);
+                    System.out.printf("Число %s НЕ счастливое", str);
+                }
             }else {
-                System.out.println(strLeft + " != " + strRight);
-                System.out.printf("Число %s НЕ счастливое", str);
+                System.out.println("Во введенной строке не все символы были числами!!!");
             }
         }else {
-            System.out.println("Вы ввели не правильное число!!!");
+            System.out.println("Во введенной строке не 6 символов!!!");
         }
     }
 }
