7bd2c1f013e7a4f487a082627770ca710e9c00c6
MikeKuzmenko81 <33359612+MikeKuzmenko81@users.noreply.github.com>
2019-11-19 19:04:41 +0300

    Update Convertion.java

--- Convertion.java
+++ Convertion.java
@@ -1,1 +1,16 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Conversion {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println();
+    }
+
+    public static double convert(double sum, double cur){
+        double result = sum * cur;
+
+        return result;
+    }
+}
