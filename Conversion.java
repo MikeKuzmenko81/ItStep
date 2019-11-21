9f2ed0d5fa21090c6d444806b6d84707e8642199
Mike_Kuzzz <mike.kmv@gmail.com>
2019-11-21 16:42:38 +0300

    nauroke

--- Conversion.java
+++ Conversion.java
@@ -3,14 +3,22 @@ package ru.mike;
 import java.util.Scanner;
 
 public class Conversion {
+    static final double EURO_TO_DOL = 1.2;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println();
+        System.out.println("Введите сумму и курс валюты");
+        double sum = scan.nextDouble();
+        double curs = scan.nextDouble();
+        double res = convert(sum, curs);
+        System.out.println(res);
     }
 
-    public static double convert(double sum, double cur){
-        double result = sum * cur;
-
+    public static double convert(double sum, double EURO_TO_DOL){
+        double result = 0;
+        double curs = EURO_TO_DOL;
+        if(sum > 0 && curs > 0) {
+            result = sum * curs;
+        }
         return result;
     }
 }
