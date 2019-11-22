54ebc73caa0c436397ddf662b7b6de783a78601c
Mike_Kuzzz <mike.kmv@gmail.com>
2019-11-22 23:29:41 +0300

    Task01

---
+++ praktik/lab07/Task01.java
@@ -0,0 +1,27 @@
+package ru.mike.praktik.lab07;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое число диапазона");
+        int num1 = scan.nextInt();
+        System.out.println("Введите второе число диапазона");
+        int num2 = scan.nextInt();
+        int numMin = 0;
+        int numMax = 0;
+        if(num1 < num2){
+            numMin = num1;
+            numMax = num2;
+        }else {
+            numMin = num2;
+            numMax = num1;
+        }
+        int counter = numMin;
+        while (counter <= numMax){
+            counter++;
+            System.out.print(counter + " | ");
+        }
+    }
+}
