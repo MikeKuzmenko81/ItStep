de4385e34792a7b5100821a297145dbf5fd912fc
Mike_Kuzzz <mike.kmv@gmail.com>
2019-12-03 09:57:48 +0300

    Task03

---
+++ dz6/Task03.java
@@ -0,0 +1,11 @@
+package ru.mike.dz6;
+
+public class Task03 {
+    public static void main(String[] args) {
+        int n = 179;
+        sumNumbers(179);
+    }
+    public static void sumNumbers (int num){
+
+    }
+}
