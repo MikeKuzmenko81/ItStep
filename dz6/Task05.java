28d7ca22ff8adc7f1e56fcf8c50cfd9dff658f09
Mike_Kuzzz <mike.kmv@gmail.com>
2019-12-08 00:10:33 +0300

    Task05

--- dz6/Task05.java
+++ dz6/Task05.java
@@ -1,4 +1,24 @@
 package ru.mike.dz6;
 
 public class Task05 {
+    public static void main(String[] args) {
+        int n = 179;
+        orderNumbersDesk(n);
+    }
+    public static void orderNumbersDesk(int number){
+        int num = 0;
+        if((number < 1000) && (number >= 100)){
+            num = number / 100;
+            number = number % 100;
+            orderNumbersDesk(number);
+            System.out.println(num);
+        }else if((number < 100) && (number >= 10)){
+            num = number / 10;
+            number = number % 10;
+            orderNumbersDesk(number);
+            System.out.println(num);
+        }else {
+            System.out.println(number);
+        }
+    }
 }
