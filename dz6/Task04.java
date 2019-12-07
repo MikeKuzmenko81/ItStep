bd38699f95c3efc8a87ac22d5eab33d819ffd599
Mike_Kuzzz <mike.kmv@gmail.com>
2019-12-08 00:08:23 +0300

    Task04

--- dz6/Task04.java
+++ dz6/Task04.java
@@ -1,5 +1,24 @@
 package ru.mike.dz6;
 
 public class Task04 {
-
+    public static void main(String[] args) {
+        int n = 179;
+        orderNumbersAsc(n);
+    }
+    public static void orderNumbersAsc (int number){
+        int num = 0;
+        if((number < 1000) && (number >= 100)){
+            num = number / 100;
+            number = number % 100;
+            System.out.println(num);
+            orderNumbersAsc (number);
+        }else if((number < 100) && (number >= 10)){
+            num = number / 10;
+            number = number % 10;
+            System.out.println(num);
+            orderNumbersAsc (number);
+        }else {
+            System.out.println(number);
+        }
+    }
 }
