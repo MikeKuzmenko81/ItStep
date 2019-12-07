472c70dba1d96da84e88dfb12c8af91d65d8d9f5
Mike_Kuzzz <mike.kmv@gmail.com>
2019-12-07 23:49:50 +0300

    Task03

--- dz6/Task03.java
+++ dz6/Task03.java
@@ -3,9 +3,22 @@ package ru.mike.dz6;
 public class Task03 {
     public static void main(String[] args) {
         int n = 179;
-        sumNumbers(179);
+        int sum = sumNumbers(n);
+        System.out.println(sum);
     }
-    public static void sumNumbers (int num){
-
+    public static int sumNumbers (int num){
+        int slag = 0;
+        int result = 0;
+        if((num < 1000) && (num >= 100)){
+            slag = num / 100;
+            num = num % 100;
+            return slag + sumNumbers(num);
+        }if((num < 100) && (num >= 10)){
+            slag = num / 10;
+            num = num % 10;
+            return slag + sumNumbers(num);
+        }else {
+            return num;
+        }
     }
 }
