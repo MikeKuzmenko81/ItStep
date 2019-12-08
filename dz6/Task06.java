02df04196cd8049d9a6022ccf6439cf416724306
Mike_Kuzzz <mike.kmv@gmail.com>
2019-12-08 19:56:54 +0300

    Task06

--- dz6/Task06.java
+++ dz6/Task06.java
@@ -6,10 +6,11 @@ public class Task06 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int numIn = scan.nextInt();
+        System.out.println("Ввод : " + numIn);
         int desGreat = findDes(numIn, 1);
         int desLess = desGreat / 10;
         int revNum = reversNum(numIn, desGreat, desLess, 1);
-        System.out.println(revNum);
+        System.out.println("Вывод : " + revNum);
 
     }
     public static int findDes(int num, int des){
@@ -22,11 +23,10 @@ public class Task06 {
     }
 
     public static int reversNum (int num, int great, int less, int mnogetel){
-        int slag = 0;
+        int slag = num / less;
         int l = less/10;
         int m = mnogetel * 10;
         if(less > 1){
-            slag = num / less;
             num = num % less;
             great /= 10;
             return (slag * mnogetel) + reversNum(num, great, l, m);
