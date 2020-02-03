300215a5917289fd5b69defcd04a65c41436daab
Mike_Kuzzz <mike.kmv@gmail.com>
2020-02-03 23:24:24 +0300

    Realizaciya

---
+++ dz10/Company.java
@@ -0,0 +1,15 @@
+package ru.mike.dz10;
+
+import java.util.Iterator;
+
+public class Company implements Iterable<Employee> {
+    private Employee[] employees = new Employee[0];
+    public void setEmployees(Employee[] employees){
+        this.employees = employees;
+    }
+
+    @Override
+    public Iterator<Employee> iterator() {
+        return null;
+    }
+}
