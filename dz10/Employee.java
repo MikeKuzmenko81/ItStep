ce2cf046cdc4839b62168efdefee77540288b067
Mike_Kuzzz <mike.kmv@gmail.com>
2020-02-03 23:49:55 +0300

    Realizaciya

--- dz10/Employee.java
+++ dz10/Employee.java
@@ -1,10 +1,19 @@
 package ru.mike.dz10;
 
+//Описание объекта который помещаем в список
 public class Employee {
     private String name;
     private int age;
     public Employee(String name, int age){
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
